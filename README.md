112c7e8bd97e7b1d52f4af921017dfd379cd7f1c
Haoli Yin <haoli.yin@vanderbilt.edu>
2023-11-29 18:25:13 -0600

    Update with new dataset and preprocessing

--- README.md
+++ README.md
@@ -4,13 +4,19 @@
 
 A botnet is defined as a network of private computers infected with malicious software and controlled as a group without the owners' knowledge. 
 
-Computer Network Traffic Data - A ~500K CSV with summary of some real network traffic data from the past. The dataset has ~21K rows and covers 10 local workstation IPs over a three month period. Half of these local IPs were compromised at some point during this period and became members of various botnets. Data is taken from the following [Stanford Public Data Repository](https://chiarasabatti.su.domains/data.html).
+We will be using the [CTU-13 dataset](https://www.stratosphereips.org/datasets-ctu13), which is large capture of real botnet traffic mixed with normal traffic and background traffic captured from CTU University, Czech Republic, in 2011. The CTU-13 dataset consists in thirteen captures (called scenarios) of different botnet samples. On each scenario, there was a specific malware executed, which used several protocols and performed different actions. More information about this dataset can be found in the link. All CSV data can be found in the *.binetflow file of each respective directory. 
 
-The columns of the CSV data are the following:
-date: yyyy-mm-dd (from 2006-07-01 through 2006-09-30)
-l_ipn: local IP (coded as an integer from 0-9)
-r_asn: remote ASN (an integer which identifies the remote ISP)
-f: flows (count of connnections for that day)
+You can get the data by running the following: 
+```bash
+cd data
+wget https://mcfp.felk.cvut.cz/publicDatasets/CTU-13-Dataset/CTU-13-Dataset.tar.bz2
+```
+Note: this data file is 1.9GB so it may take some time to download. 
+
+And then unzip it:
+```bash
+tar -xvjf CTU-13-Dataset.tar.bz2
+```
 
 ## Setup 
 
---
+++ preprocessing.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import os
+import pickle
+import datetime
+import itertools
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+
+from utils import *
+
+PARENT_PATH = 'data'
+PREFIX_PATH = f'{PARENT_PATH}/CTU-13-Dataset'
+
+def loadData(train_files):
+    dfs = []
+    for file in train_files:
+        path = os.path.join(PREFIX_PATH, file)
+        dfs.append(pd.read_csv(path))
+    all_data = pd.concat(dfs)  # Concat all to a single df
+    return all_data
+
+def preprocessing(data):
+    data.columns = data.columns.str.lower()
+
+    data['target'] = data['label'].apply(convertLabel)
+    data['starttime'] = pd.to_datetime(data['starttime'])
+
+    # We replace Null value to Con 
+    data['state'] = data.state.fillna(value='CON')
+
+    # fill nan port forward 
+    data['sport'] = data.sport.fillna(method='pad')
+    data['dport'] = data.dport.fillna(method='pad')
+
+    data['stos'] = data.stos.fillna(value=0.0)
+    data['dtos'] = data.dtos.fillna(value=0.0)
+
+    data = data.drop(data[data.stos == 192.0].index)
+    return data
+
+if __name__ == '__main__':
+    test_files = ['capture20110811.binetflow','capture20110810.binetflow',\
+        'capture20110816-2.binetflow','capture20110816.binetflow',\
+        'capture20110818-2.binetflow','capture20110819.binetflow']
+
+    train_files = []
+    for folder in os.listdir(PREFIX_PATH):
+        for file in os.listdir(os.path.join(PREFIX_PATH, folder)):
+            if file.endswith('.binetflow'):
+                train_files.append(os.path.join(folder, file))
+            if file in test_files: continue
+    
+    all_data = loadData(train_files)
+    all_data = preprocessing(all_data)
+    
+    train, test = train_test_split(all_data, test_size=0.3, random_state=42)
+
+    train_0, train_1 = train['target'].value_counts()[0] / len(train.index), train['target'].value_counts()[1] / len(train.index)
+    test_0, test_1 = test['target'].value_counts()[0] / len(test.index), test['target'].value_counts()[1] / len(test.index)
+
+    print("In Train: there are {} % of class 0 and {} % of class 1".format(train_0, train_1))
+    print("In Test: there are {} % of class 0 and {} % of class 1".format(test_0, test_1))
+
+
+    train.to_csv(f'{PARENT_PATH}/train_alldata_EDA.csv')
+    test.to_csv(f'{PARENT_PATH}/test_alldata_EDA.csv')
